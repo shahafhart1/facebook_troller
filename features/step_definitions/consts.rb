6d950485ccad1d5fe73328b1c97d17005464e61f
Shahaf Hart <shahaf.hart@wiser.com>
2015-06-16 11:17:41 +0300

    adding some more sentances and profiles

--- features/step_definitions/consts.rb
+++ features/step_definitions/consts.rb
@@ -2,4 +2,8 @@ $karnivor_sentances = ["בשר זה טעים רצח","מממ... סטייק"]
 $tivoni_sentances = ["בשר זה רצחחחחח","אתם כולכם רוצחים"]
 $smolani_sentances = ["ליכודניקים חסרי תרבות","מירי רגב בהמה"]
 $yemani_sentances = ["כהנא צדק","מירי רגב שולטתתתת"]
+$feminist_sentances = ["זו ההגדרה של פטריארכיה","האשמת הקורבן!"]
+$shobinist_sentances = ["תחזרו למטבח","אין מה לעשות, יש דברים שנשים פחות טובות בהם"]
 
+$facebook_profiles = ["wisertroll1@gmail.com","hacktroller1@gmail.com"]
+$facebook_password = "Wiser123"
